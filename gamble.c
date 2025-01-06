cd9583938d56d9a413566642f0d24be3a1348c5c
Chris Lam <clam50@stuy.edu>
2025-01-06 15:24:47 -0500

    random number function

--- gamble.c
+++ gamble.c
@@ -1,5 +1,16 @@
 #include gamble.c
 
-void playgame(){
-  
+int create_prize(){
+  int data = open("arraydata.dat", O_CREAT | O_RDWR, 0640);
+  if(read(data, &data, sizeof(data)) < 0){
+    perror("mkfifo");
+    exit(1);
+  }
+  return data;
+}
+
+void play_game(){
+  int prize = create_prize();
+  printf("Total prize: %d \n", prize);
+  printf("You will have the next 10 seconds to discuss")
 }
